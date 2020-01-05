223be024a92fbc6e93c5af8e52aab96cdf069a75
Jesse Kuntz <jkuntz@groupon.com>
2020-01-04 21:30:59 -0600

    feat: single accordion open at a time

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var accordions = [...document.getElementsByClassName('accordion')];
+var panels = [...document.getElementsByClassName('panel')];
+
+function resetAccordions(currentAccordion) {
+    const otherAccordions = accordions.filter(accordion => { return accordion != currentAccordion })
+    otherAccordions.forEach(otherAccordion => {
+        otherAccordion.classList.remove('active');
+        otherAccordion.nextElementSibling.style.display = 'none';
+    });
+}
 
 accordions.forEach(accordion => {
     accordion.addEventListener('click', function() {
+        resetAccordions(accordion);
+
         this.classList.toggle('active');
 
         var panel = this.nextElementSibling;
