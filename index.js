dc14ddee5cd5b1d573bf6faa85cf599a14cc7a49
Jesse Kuntz <jkuntz@groupon.com>
2020-05-02 11:08:29 -0500

    feat: dark mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var accordions = [...document.getElementsByClassName('accordion')];
-var panels = [...document.getElementsByClassName('panel')];
+const accordions = [...document.getElementsByClassName('accordion')];
+const panels = [...document.getElementsByClassName('panel')];
+const darkModeCat = document.querySelector('.dark-mode-cat');
 
 function resetAccordions(currentAccordion) {
     const otherAccordions = accordions.filter(accordion => { return accordion != currentAccordion })
@@ -28,4 +29,19 @@ accordions.forEach(accordion => {
             'top': accordion.offsetTop
         });
     });
+});
+
+darkModeCat.addEventListener('click', () => {
+    const root = document.documentElement;
+    const rootStyles = getComputedStyle(root);
+
+    const darkColor = rootStyles.getPropertyValue('--dark-color');
+    const lightColor = rootStyles.getPropertyValue('--light-color');
+    const darkModeImage = rootStyles.getPropertyValue('--dark-mode-cat');
+    const lightModeImage = rootStyles.getPropertyValue('--light-mode-cat');
+
+    root.style.setProperty('--dark-color', lightColor);
+    root.style.setProperty('--light-color', darkColor);
+    root.style.setProperty('--dark-mode-cat', lightModeImage);
+    root.style.setProperty('--light-mode-cat', darkModeImage);
 });
