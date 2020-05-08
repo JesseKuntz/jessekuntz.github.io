81393612ba1b9b122e109f5f7bd2c4bed6da377b
Jesse Kuntz <jkuntz@groupon.com>
2020-05-07 19:29:00 -0500

    feat: project cards

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-const accordions = [...document.getElementsByClassName('accordion')];
+const cards = [...document.getElementsByClassName('card')];
 const panels = [...document.getElementsByClassName('panel')];
 const darkModeCat = document.querySelector('.dark-mode-cat');
 
-function resetAccordions(currentAccordion) {
-    const otherAccordions = accordions.filter(accordion => { return accordion != currentAccordion })
-    otherAccordions.forEach(otherAccordion => {
-        otherAccordion.classList.remove('active');
-        otherAccordion.nextElementSibling.style.display = 'none';
-    });
+function resetCards(currentCard) {
+  const otherCards = cards.filter(card => { return card != currentCard })
+  otherCards.forEach(otherCard => {
+    otherCard.classList.remove('active');
+    otherCard.style.height = '240px';
+  });
 }
 
-accordions.forEach(accordion => {
-    accordion.addEventListener('click', function() {
-        resetAccordions(accordion);
+cards.forEach(card => {
+  card.addEventListener('click', function () {
+    resetCards(card);
 
-        this.classList.toggle('active');
+    this.classList.toggle('active');
 
-        var panel = this.nextElementSibling;
-        if (panel.style.display === 'block') {
-            panel.style.display = 'none';
-        } else {
-            panel.style.display = 'block';
-        }
+    if (card.style.height === '100%') {
+      card.style.height = '240px';
+    } else {
+      card.style.height = '100%';
+    }
 
-        window.scrollTo({
-            'behavior': 'smooth',
-            'left': 0,
-            'top': accordion.offsetTop
-        });
+    window.scrollTo({
+      'left': 0,
+      'top': card.offsetTop - 20
     });
+  });
 });
 
 darkModeCat.addEventListener('click', () => {
-    const root = document.documentElement;
-    const rootStyles = getComputedStyle(root);
+  const root = document.documentElement;
+  const rootStyles = getComputedStyle(root);
 
-    const darkColor = rootStyles.getPropertyValue('--dark-color');
-    const lightColor = rootStyles.getPropertyValue('--light-color');
-    const darkModeImage = rootStyles.getPropertyValue('--dark-mode-cat');
-    const lightModeImage = rootStyles.getPropertyValue('--light-mode-cat');
+  const darkColor = rootStyles.getPropertyValue('--dark-color');
+  const lightColor = rootStyles.getPropertyValue('--light-color');
+  const darkModeImage = rootStyles.getPropertyValue('--dark-mode-cat');
+  const lightModeImage = rootStyles.getPropertyValue('--light-mode-cat');
+  const linkLightColor = rootStyles.getPropertyValue('--link-light-color');
+  const linkVisitedLightColor = rootStyles.getPropertyValue('--link-visited-light-color');
+  const linkDarkColor = rootStyles.getPropertyValue('--link-dark-color');
+  const linkVisitedDarkColor = rootStyles.getPropertyValue('--link-visited-dark-color');
 
-    root.style.setProperty('--dark-color', lightColor);
-    root.style.setProperty('--light-color', darkColor);
-    root.style.setProperty('--dark-mode-cat', lightModeImage);
-    root.style.setProperty('--light-mode-cat', darkModeImage);
+  root.style.setProperty('--dark-color', lightColor);
+  root.style.setProperty('--light-color', darkColor);
+  root.style.setProperty('--dark-mode-cat', lightModeImage);
+  root.style.setProperty('--light-mode-cat', darkModeImage);
+  root.style.setProperty('--link-light-color', linkDarkColor);
+  root.style.setProperty('--link-visited-light-color', linkVisitedDarkColor);
+  root.style.setProperty('--link-dark-color', linkLightColor);
+  root.style.setProperty('--link-visited-dark-color', linkVisitedLightColor);
 });
