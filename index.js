e1b72485c17c7a1bf1418429b350e0e5c873c2a6
Jesse Kuntz <jkuntz@groupon.com>
2020-05-10 09:19:24 -0500

    fix: only apply fade to those that need it

--- index.js
+++ index.js
@@ -2,36 +2,24 @@ const cards = [...document.getElementsByClassName('card')];
 const panels = [...document.getElementsByClassName('panel')];
 const darkModeCat = document.querySelector('.dark-mode-cat');
 
+function shouldHaveFade(card) {
+  if (card.scrollHeight > card.offsetHeight) {
+    return true;
+  }
+
+  return false;
+}
+
 function resetCards(currentCard) {
-  const otherCards = cards.filter(card => { return card != currentCard })
+  const otherCards = cards.filter(card => {
+    return (card !== currentCard) && (shouldHaveFade(card) || card.classList.value.includes('active'))
+  })
   otherCards.forEach(otherCard => {
     otherCard.classList.remove('active');
     otherCard.style.height = '240px';
     otherCard.lastElementChild.style.display = 'block';
   });
 }
-
-cards.forEach(card => {
-  card.addEventListener('click', function () {
-    resetCards(card);
-
-    this.classList.toggle('active');
-    const fade = card.querySelector('.fade');
-
-    if (card.style.height === '100%') {
-      card.style.height = '240px';
-      fade.style.display = 'block';
-    } else {
-      card.style.height = '100%';
-      fade.style.display = 'none';
-    }
-
-    window.scrollTo({
-      'left': 0,
-      'top': card.offsetTop - 20
-    });
-  });
-});
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -42,6 +30,32 @@ function meow() {
   const audio = new Audio(`assets/dark-mode/meow${random}.mp3`);
   audio.play();
 }
+
+cards.forEach(card => {
+  if (shouldHaveFade(card)) {
+    card.addEventListener('click', function () {
+      resetCards(card);
+
+      this.classList.toggle('active');
+      const fade = card.querySelector('.fade');
+
+      if (card.style.height === '100%') {
+        card.style.height = '240px';
+        fade.style.display = 'block';
+      } else {
+        card.style.height = '100%';
+        fade.style.display = 'none';
+      }
+
+      window.scrollTo({
+        'left': 0,
+        'top': card.offsetTop - 20
+      });
+    });
+
+    card.lastElementChild.style.display = 'block';
+  }
+});
 
 darkModeCat.addEventListener('click', () => {
   meow();
