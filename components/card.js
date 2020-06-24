79bbc4d0856d351db1e4d2bec5a51873d4c5596f
Jesse Kuntz <jessekuntz96@gmail.com>
2020-06-23 20:59:58 -0500

    feat: componentize project cards

---
+++ components/card.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class ProjectCard extends HTMLElement {
+  constructor() {
+    super();
+
+    const title = this.getAttribute('title');
+    const emoji = this.getAttribute('emoji');
+    const content = this.innerHTML;
+
+    this.innerHTML = `
+      <button class='card' id='model-creator'>
+        <div class='title-wrapper'>
+          <h3 class='card-title'>
+            <span>${title}</span>
+            <span>${emoji}</span>
+          </h3>
+        </div>
+        <div class='content'>
+          ${content}
+        </div>
+      </button>
+    `;
+
+    this.className = 'card-wrapper'
+  }
+}
+
+window.customElements.define('project-card', ProjectCard);
