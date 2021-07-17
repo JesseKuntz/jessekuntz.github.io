e4f093e18e9635c05b10a7acda3a4930f3624205
JesseKuntz <jessekuntz96@gmail.com>
2021-07-17 17:05:11 -0500

    perf: preload hero image

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]',
+        },
       },
     ],
   },
